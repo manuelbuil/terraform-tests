712e0156a6a376090517e79d6f548947da7d7cea
root <mbuil@suse.com>
2019-12-17 12:12:47 +0000

    More servers

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_instance" "example" {
-  ami		= "ami-087855b6c8b59a9e4"
+resource "aws_launch_configuration" "example" {
+  image_id		= "ami-087855b6c8b59a9e4"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -13,8 +13,22 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -38,4 +52,8 @@ variable "server_port" {
 output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP address of the web server"
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
